55e7a0749b810aaae408c24eb65258baf7062482
Jason Yu <jasonyu0722@gmail.com>
2026-04-13 17:09:18 -0400

    feat: create hashing functions

--- src/incremental/__init__.py
+++ src/incremental/__init__.py
@@ -1,3 +1,10 @@
 from .state_store import StateStore
+from .hashing import hash_chunk, hash_section, hash_document, hash_components
 
-__all__ = ["StateStore"]
+__all__ = [
+    "StateStore",
+    "hash_chunk",
+    "hash_section",
+    "hash_document",
+    "hash_components",
+]
